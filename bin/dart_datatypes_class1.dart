cb13ce3d8dee6f5019babb7bee3640fb5e3dcc19
raja226-kanini <144207791+raja226-kanini@users.noreply.github.com>
2024-01-04 23:58:00 +0530

    variables and datatypes basics

---
+++ bin/dart_datatypes_class1.dart
@@ -0,0 +1,9 @@
+void main() 
+{
+  //Datatype variable name = value ;
+  //variable name was in lower case rules
+ int age = 33;
+ String name = "Rajasekhar Gogula";
+ print('My age: $age \nMy Name : $name');
+
+}
