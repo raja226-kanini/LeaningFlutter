986f0a218f78b036f07f117232a1dd5b95fbbe1f
raja226-kanini <144207791+raja226-kanini@users.noreply.github.com>
2024-01-04 23:41:14 +0530

    hellow world day1

---
+++ bin/dart_samplepractice_console.dart
@@ -0,0 +1,6 @@
+import 'package:dart_samplepractice_console/dart_samplepractice_console.dart' as dart_samplepractice_console;
+
+void main(List<String> arguments) {
+  String name = "rajasekhar gogula";
+  print('Hello world: $name ? "true value" : "Novalue');
+}
